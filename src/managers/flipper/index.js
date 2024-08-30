ea06fc1c19fc43196d287d7d051264e0c070e73a
Theo Tanase <theodor.tanase@ilteducation.com>
2024-08-30 12:35:47 +0200

    add another page after

--- src/managers/flipper/index.js
+++ src/managers/flipper/index.js
@@ -16,12 +16,70 @@ class FlipperManager extends DefaultViewManager {
 			});
 	}
 
+	addAnotherPageAfter() {
+		const lastView = this.views.last();
+		const nextSection = lastView && lastView.section.next();
+		const forceRight = false;
+
+		if(nextSection) {
+			return this.append(nextSection, forceRight)
+				.then(() => {
+					return this.handleNextPrePaginated(forceRight, nextSection, this.append);
+				})
+				.then(() => {
+					this.views.show();
+				});
+		}
+
+		return Promise.resolve();
+	}
+
 	fill() {
+		console.log("will try to fill the other pages");
+
+
+		this.q.enqueue(() => {
+			return this.addAnotherPageAfter();
+		});
+
+		return Promise.resolve();
+
+
 
 		// Add one more page after this one
 
+		// const nextSection = this.view.section.next();
+		//
+		// // TODO - figure out force right
+		// const forceRight = false;
 
-		console.log("will try to fill the other pages");
+
+
+		if(nextSection) {
+
+
+			return this.append(nextSection, forceRight)
+				.then(function(){
+					return this.handleNextPrePaginated(forceRight, nextSection, this.append);
+				}.bind(this), (err) => {
+					return err;
+				})
+				.then(function(){
+
+					// Reset position to start for scrolled-doc vertical-rl in default mode
+					if (!this.isPaginated &&
+						this.settings.axis === "horizontal" &&
+						this.settings.direction === "rtl" &&
+						this.settings.rtlScrollType === "default") {
+
+						this.scrollTo(this.container.scrollWidth, 0, true);
+					}
+					this.views.show();
+				}.bind(this));
+		}
+
+
+		return Promise.resolve();
 	}
 }
 
